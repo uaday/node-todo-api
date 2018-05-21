a28bb2dc27eceb89a1b786e21738defee16e680c
dev <appinion.dev@gmail.com>
2018-05-21 18:59:07 +0600

    update not working

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
-var {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -80,7 +81,29 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send(e);
 });
 
+app.patch('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    var body=_.pick(res.body,['text','completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
 
+    if(_.isBoolean(body.completed)&&body.completed){
+        body.completed=new Date().getTime();
+    }else{
+        body.completed=false;
+        body.completed=null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+});
 
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
