e9b0b316f24df7287ff7f2f5714d2d9d986b3d45
uaday ghosh <uadayghosh@gmail.com>
2018-05-23 03:11:49 +0600

    authantication working

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,33 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
-var user = mongoose.model('User', {
+var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique:true,
+        validate:{
+            validator:validator.isEmail,
+            message:'{value is not valid}'
+        }
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength:6
+    },
+    tokens:[{
+        access:{
+            type:String,
+            require:true
+        },
+        token:{
+            type:String,
+            require:true
+        }
+    }]
 });
 
-module.exports={user};
+module.exports={User};
