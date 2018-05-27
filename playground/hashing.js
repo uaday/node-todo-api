fef1a2c70835eab4ecd65958755e1718c2f26e9d
uaday ghosh <uadayghosh@gmail.com>
2018-05-28 02:58:39 +0600

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
-const {SHA256}=require('crypto-js');
+const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data={
-    id:10
-};
+var password = '123abc#';
 
-var token = jwt.sign(data,'123Abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token,'123Abc');
-console.log('decoded',decoded);
+var hashedPassword = '$2a$10$ckocF6koOKhoya2DsHhOIubofODGrQhT33Ula3z7KUkjWU9lTuOpe';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+console.log(res);
+});
+
+// var data={
+//     id:10
+// };
+
+// var token = jwt.sign(data,'123Abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123Abc');
+// console.log('decoded',decoded);
 
 
 // var message = 'hello world';
@@ -29,5 +43,5 @@ console.log('decoded',decoded);
 //     console.log('Data was not changed');
 // }else{
 //     console.log('Data was changed. Do not truest them');
-    
+
 // }
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
+
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -54,9 +56,9 @@ UserSchema.methods.generateAuthToken = function () {
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
-    try{
-        decoded=jwt.verify(token,'abc123');
-    }catch(e){
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
         // return new Promise((resolve,reject)=>{
         //     reject();
         // });
@@ -64,11 +66,25 @@ UserSchema.statics.findByToken = function (token) {
     }
 
     return User.findOne({
-        '_id':decoded._id,
-        'tokens.token':token,
-        'tokens.access':'auth'
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 }
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
